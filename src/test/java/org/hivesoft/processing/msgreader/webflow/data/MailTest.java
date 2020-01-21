c42905fa15008e5be05f5f628e3d6b8d788f244f
drohne1673 <drohne1673@gmx.net>
2020-01-22 00:24:14 +0100

    added a empty test

--- src/test/java/org/hivesoft/processing/msgreader/webflow/data/MailTest.java
+++ src/test/java/org/hivesoft/processing/msgreader/webflow/data/MailTest.java
@@ -2,9 +2,13 @@ package org.hivesoft.processing.msgreader.webflow.data;
 
 import org.hivesoft.processing.msgreader.webflow.testutils.AbstractMarshallingTestBase;
 import org.junit.jupiter.api.Test;
+import org.springframework.binding.message.Severity;
 import org.springframework.webflow.test.MockRequestContext;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,7 +30,7 @@ class MailTest extends AbstractMarshallingTestBase {
   }
 
   @Test
-  void parseMail_invalid() {
+  void parseMail_notValid() {
     classUnderTest = new Mail("just some text");
 
     Collection<MailHeader> parsedMail = classUnderTest.parseMail(new MockRequestContext());
@@ -34,5 +38,18 @@ class MailTest extends AbstractMarshallingTestBase {
     assertThat(parsedMail)
             .contains(new MailHeader("just some text", "just some text"))
             .hasSize(1);
+  }
+
+  @Test
+  void parseMail_empty() {
+    classUnderTest = new Mail(null);
+
+    MockRequestContext context = new MockRequestContext();
+    Collection<MailHeader> parsedMail = classUnderTest.parseMail(context);
+
+    assertThat(parsedMail)
+            .isEmpty();
+    List<String> allMessages = Arrays.stream(context.getMessageContext().getAllMessages()).map(m -> m.getText() + " " + m.getSeverity()).collect(Collectors.toList());
+    assertThat(allMessages).contains("Please enter a valid source!" + " " + Severity.ERROR);
   }
 }
